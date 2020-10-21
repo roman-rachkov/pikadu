e57d51fd6c912eab7ff5c442aaa36846af381b7a
Roman Rachkov <pakycb84@gmail.com>
2020-10-21 18:18:36 +1000

    articles complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import "./js/common"
-import "./assets/scss/main.scss"
-import "./assets/css/main.css"
+
 
 // import "../node_modules/jquery/dist/jquery.min"
 //
 import "../node_modules/materialize-css/dist/js/materialize.min"
 import "../node_modules/materialize-css/dist/css/materialize.min.css"
 
+import "./js/common"
+import "./assets/scss/main.scss"
+import "./assets/css/main.css"
