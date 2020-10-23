1aa947ca7b36e1f809ef6896e45e02c7b30b19a6
Roman Rachkov <pakycb84@gmail.com>
2020-10-23 16:02:14 +1000

    addaptive complite

--- src/js/common.js
+++ src/js/common.js
@@ -1,11 +1,3 @@
-let add = (a,b) => a+b
-console.log(add(3,2));
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.datepicker');
-//     var instances = M.Datepicker.init(elems);
-// });
-
 jQuery(document).ready(function($){
-    $('.datepicker').datepicker();
+    $('.sidenav').sidenav();
 });
